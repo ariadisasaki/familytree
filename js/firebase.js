87d6f232f62c57aa641cffc44a48887a281d3784
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-18 05:53:41 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,3 @@
-// =============================
-// FIREBASE INIT (SHARED)
-// =============================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 

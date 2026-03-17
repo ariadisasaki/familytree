0b2c6e3b95eb176db2f7d145f0486750ce765c92
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-18 00:06:49 +0800

    Create profile.js

---
+++ js/profile.js
@@ -0,0 +1,40 @@
+import { db }
+
+from "./firebase.js"
+
+import { doc, getDoc }
+
+from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js"
+
+const params = new URLSearchParams(window.location.search)
+
+const id = params.get("id")
+
+async function load(){
+
+const ref = doc(db,"anggota_keluarga",id)
+
+const snap = await getDoc(ref)
+
+const d = snap.data()
+
+document.getElementById("profile").innerHTML=
+
+`
+<img src="${d.foto_url || ''}" width="150">
+
+<h2>${d.nama}</h2>
+
+<p>Ayah : ${d.ayah_id || '-'}</p>
+
+<p>Ibu : ${d.ibu_id || '-'}</p>
+
+<p>Pasangan : ${d.pasangan_id || '-'}</p>
+
+<p>Tahun Lahir : ${d.tanggal_lahir || '-'}</p>
+
+`
+
+}
+
+load()
